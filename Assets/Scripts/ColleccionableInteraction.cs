7e8992ff5b6be10ac7de7b21bc5864feba32a00e
Sanca Venegas, Martin Joel <splayer003@gmail.com>
2021-01-31 12:57:10 -0500

    bug fix lvl2

--- Assets/Scripts/ColleccionableInteraction.cs
+++ Assets/Scripts/ColleccionableInteraction.cs
@@ -6,13 +6,14 @@ using UnityEngine.Tilemaps;
 public class ColleccionableInteraction : MonoBehaviour
 {
     [SerializeField] AudioSource audioSource;
+    [SerializeField] GameObject noteTile;
 
     private void OnTriggerEnter2D(Collider2D collision) {
         if (collision.gameObject.tag == "Player") {
             GetComponent<Collider2D>().enabled = false;
             Debug.Log("collect");
             audioSource.Play();
-            Destroy(transform.GetChild(0));
+            Destroy(noteTile);
         }
     }
 }
