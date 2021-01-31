005751226129f8fb565efc1037549f2f8858199b
Richard Diaz <radiazg@pucp.pe>
2021-01-31 02:10:09 -0500

    Generic message object

---
+++ Assets/Scripts/GenericDialogObject.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenericDialogObject : MonoBehaviour
+{
+    [SerializeField] Canvas messageCanvas;
+    [SerializeField] string[] messages;
+
+    TriggerGenericDialog messageInteraction;
+    int index = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        messageInteraction = messageCanvas.GetComponent<TriggerGenericDialog>();
+    }
+    private void Update()
+    {
+        ShowNextMessage();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && messages.Length > 0)
+        {
+            messageInteraction.DisplayGenericDialog(messages[0]);
+            index += 1;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        messageInteraction.CloseMessage();
+        index = 0;
+    }
+    private void ShowNextMessage()
+    {
+        if (Input.GetButtonDown("Fire2"))
+        {
+            if(index < messages.Length)
+            {
+                messageInteraction.DisplayGenericDialog(messages[index]);
+                index += 1;
+            }
+            else
+            {
+                messageInteraction.CloseMessage();
+                StartCoroutine("DestroyThisObject");
+            }
+        }
+    }
+
+
+    IEnumerator DestroyThisObject()
+    {
+        yield return new WaitForSeconds(0.6f);
+        GameObject.Destroy(this.gameObject);
+    }
+
+}
---
+++ Assets/Scripts/TriggerGenericDialog.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class TriggerGenericDialog : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI textmeshPro;
+    [SerializeField] Image dialogBox;
+    public void CloseMessage()
+    {
+        dialogBox.gameObject.SetActive(false);
+    }
+
+    public void DisplayGenericDialog(string message)
+    {
+        dialogBox.gameObject.SetActive(true);
+        textmeshPro.SetText(message);
+    }
+}
